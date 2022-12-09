8299686c3b96adb3c45c963df6d1557422ce99ef
mykmicheals <mykmicheals@gmail.com>
2022-12-09 13:44:32 +0100

    router dependency fix

--- pages/_app.js
+++ pages/_app.js
@@ -8,29 +8,29 @@ import Spinner from '../src/components/Spinner'
 
 
 function MyApp({ Component, pageProps }) {
- 
 
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    Router.events.on("routeChangeStart", () => setLoading(true));
-    Router.events.on("routeChangeComplete", () => setLoading(false));
-    Router.events.on("routeChangeError", () => setLoading(false));
-    return () => {
-      Router.events.off("routeChangeStart", () => setLoading(true));
-      Router.events.off("routeChangeComplete", () => setLoading(false));
-      Router.events.off("routeChangeError", () => setLoading(false));
-    };
-  }, [Router.events])
+
+  // const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  //   Router.events.on("routeChangeStart", () => setLoading(true));
+  //   Router.events.on("routeChangeComplete", () => setLoading(false));
+  //   Router.events.on("routeChangeError", () => setLoading(false));
+  //   return () => {
+  //     Router.events.off("routeChangeStart", () => setLoading(true));
+  //     Router.events.off("routeChangeComplete", () => setLoading(false));
+  //     Router.events.off("routeChangeError", () => setLoading(false));
+  //   };
+  // }, [Router.events])
 
 
 
   return <Provider store={store}>
-    {loading ? (
+    {/* {loading ? (
       // <Spinner />
     <p>loafing</p>
-    ) : (
-      <Component {...pageProps} />
-    )}
+    ) : ( */}
+    <Component {...pageProps} />
+    {/* )} */}
 
 
   </Provider>
